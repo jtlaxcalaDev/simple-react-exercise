b39a558b6eaf882d5769467e75a38b889525a02f
jtlaxcala <jtlaxcala.developer@gmail.com>
2022-10-12 20:03:09 -0500

    Exercise-9-Working-with-an-API

---
+++ src/exercises/exercise-nine/ItemCard.jsx
@@ -0,0 +1,25 @@
+import './Nine.css'
+
+const ItemCard = ({username, avatar, email, name, title, skill, phone}) => {
+  return (
+    <div className="item-card">
+      <div className="item-card-inner">
+        <div className="flip-front">
+          <div className='card-header'>{username}</div>
+          <div className="card-body">
+              <img className='avatar' src={avatar} alt="user avatar" />
+          </div>
+          <div className='card-footer'>{title}</div>
+        </div>
+        <div className="flip-back">
+          <div className='row-info'><b>Name:</b> {name}</div> 
+          <div className='row-info'><b>Skill:</b> {skill}</div>
+          <div className='row-info'><b>Email:</b> {email}</div>
+          <div className='row-info'><b>Phone:</b> {phone}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ItemCard;
--- src/exercises/exercise-nine/Nine.jsx
+++ src/exercises/exercise-nine/Nine.jsx
@@ -1,12 +1,62 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ItemCard from './ItemCard'
 
 const Nine = () => {
+
+  const [itemlist, setItemlist] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if(isLoading){
+      async function getItems() {
+        try {
+          const res = await fetch('https://random-data-api.com/api/users/random_user?size=10')
+
+          if (res.ok) {
+            const data = await res.json();
+            setItemlist(data);
+            setError(null);
+            setIsLoading(false);
+          } else {
+            setError("Hubo un error al obtener los datos");
+          }
+        }catch (error) {
+          setError('Hubo un problema al obtener los datos')
+        }
+      }
+      getItems()
+    }
+  },[isLoading])
+
+  function handleLoadingChange(){
+    setIsLoading(true)
+  }
+
   return (
     <>
       <div className="container">
         <Link to={'/'} className="return">Return to home</Link>
         <div className={"exercise-title exercise-nine"}>
-          <h2>Exercise nine</h2>
+          <h2>Exercise 9: Working with an API</h2>
+        </div>
+        <button className="reload-button" onClick={handleLoadingChange}> Reload Fetch Random</button>
+        <div className="items-list-container">
+          {
+            itemlist.map(item => (
+              <ItemCard 
+              key={item.id} 
+              username={item.username} 
+              avatar={item.avatar} 
+              title={item.employment.title}
+              name={`${item.first_name} ${item.last_name}`}
+              skill={item.employment.key_skill}
+              email={item.email}
+              phone={item.phone_number}
+              />
+            ))
+          }
         </div>
       </div>
     </>
