c3761a07773c63ac7df886428ee08465a4c5132c
jtlaxcala <jtlaxcala.developer@gmail.com>
2022-11-02 11:07:27 -0600

    Exercise-4-Add-type-to-usestate

--- src/exercises/exercise-four/Four.tsx
+++ src/exercises/exercise-four/Four.tsx
@@ -5,7 +5,7 @@ import { FunctionComponent, useState } from 'react'
 
 const Four: FunctionComponent = () => {
   
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
